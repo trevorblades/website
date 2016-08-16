393a319f502ea804bad9b01f3c95419f6f68a772
Trevor Blades <tdblades@gmail.com>
2016-08-15 20:25:50 -0700

    Some stuff I forgot

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 var SRC_DIR = __dirname + '/src';
 var DEV_DIR = __dirname + '/dev';
