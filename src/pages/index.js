4daafb4d96512d0b407fe678b6700af726a021a6
Trevor Blades <tdblades@gmail.com>
2019-11-15 22:33:07 -0800

    Add streaming section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Hero from '../components/hero';
 import OpenSource from '../components/open-source';
 import Projects from '../components/projects';
 import React, {Fragment} from 'react';
-import {Box, Divider, Link, Typography} from '@material-ui/core';
+import {Box, Link, Typography} from '@material-ui/core';
 import {Helmet} from 'react-helmet';
 import {ReactComponent as Logo} from 'twemoji/2/svg/1f52a.svg';
 
@@ -24,11 +24,21 @@ export default function Home() {
         <OpenSource my={SECTION_PADDING} />
         <Accolades />
       </Box>
-      <Divider />
+      <Box p={SECTION_PADDING} color="black" bgcolor="#9146ff">
+        <Typography variant="overline" display="block">
+          F#*! it, we&apos;ll
+        </Typography>
+        <Typography gutterBottom variant="h2">
+          do it live
+        </Typography>
+        <Typography gutterBottom variant="h6">
+          Live coding Wednesdays at 7pm PT on Twitch
+        </Typography>
+      </Box>
       <Box p={SECTION_PADDING} textAlign="center">
         <Box mb={SECTION_PADDING}>
           <Typography gutterBottom variant="h3">
-            Call me maybe
+            Call me maybe?
           </Typography>
           <Typography>
             Want to get in touch? Send me an email at{' '}
