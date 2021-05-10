f7b32497747c43f651529a98cc67bdac71b65f0a
Trevor Blades <tdblades@gmail.com>
2021-05-09 23:59:54 -0700

    Add a good example text

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,23 +2,21 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {Center} from '@chakra-ui/react';
 
-function Spiral({sides, width, spacing, segments}) {
-  // shoutout
-  // https://www.mathsisfun.com/geometry/interior-angles-polygons.html
-  // https://educationisaround.com/sohcahtoa/
-  // right triangle with two equal sides: http://www.math.com/school/subject3/lessons/S3U3L4DP.html#:~:text=A%20right%20triangle%20can%20also,that%20is%20an%20isosceles%20triangle.
-  // bounding box of ngon: http://calcresource.com/geom-ngon.html
-  const sumAngles = (sides - 2) * 180;
-  const interiorAngle = sumAngles / sides;
-  const inset = Math.cos(interiorAngle * (Math.PI / 180)) * spacing * 2;
+function Spiral({size, sides, spacing, segments}) {
   const centralAngle = (Math.PI * 2) / sides;
-  const sideLength = (width / 2) * (Math.sin(centralAngle / 2) * 2);
+  const circumradius = size / 2;
+  const sideLength = circumradius * Math.sin(centralAngle / 2) * 2;
+  const inset = Math.cos(Math.PI - centralAngle) * spacing * 2;
+  const inradius = Math.cos(centralAngle / 2) * circumradius;
+  const height = sides % 2 ? inradius + circumradius : inradius * 2;
+  const spacingRatio = spacing / size;
+  console.log(height);
   return (
     <div
       style={{
-        width,
-        height: width,
-        padding: `0 ${(width - sideLength) / 2}px`,
+        width: size,
+        height: size,
+        padding: `${(size - height) / 2}px ${(size - sideLength) / 2}px`,
         overflow: 'hidden'
       }}
     >
@@ -26,14 +24,12 @@ function Spiral({sides, width, spacing, segments}) {
         .slice()
         .reverse()
         .reduce((child, value, index, array) => {
-          const offset = array.length - index;
+          const side = array.length - index;
+          const [a, b, c] = Array.from({length: 3}, (value, index) =>
+            Math.max(Math.floor((side - index) / sides), 0)
+          );
+          const innerWidth = sideLength - (a + c) * spacing - inset * b;
 
-          const ahead = Math.floor(offset / sides);
-          const middle = Math.max(Math.floor((offset - 1) / sides), 0);
-          const behind = Math.max(Math.floor((offset - 2) / sides), 0);
-          const combined = ahead + behind;
-
-          const innerWidth = sideLength - combined * spacing - inset * middle;
           if (innerWidth < spacing) {
             return null;
           }
@@ -43,7 +39,10 @@ function Spiral({sides, width, spacing, segments}) {
               style={{
                 display: 'flex',
                 transformOrigin: 'left',
-                transform: offset > 1 && `rotate(${180 - interiorAngle}deg)`
+                transform:
+                  side > 1
+                    ? `rotate(${centralAngle * (180 / Math.PI)}deg)`
+                    : 'translateY(-50%)'
               }}
             >
               <span
@@ -53,8 +52,7 @@ function Spiral({sides, width, spacing, segments}) {
                   whiteSpace: 'pre',
                   justifyContent: 'space-evenly',
                   width: innerWidth,
-                  padding: `0 ${(innerWidth * (spacing / width)) / 2}px`,
-                  backgroundColor: 'paleturquoise'
+                  padding: `0 ${(innerWidth * spacingRatio) / 2}px`
                 }}
               >
                 {value.split('').map((char, index) => (
@@ -71,7 +69,7 @@ function Spiral({sides, width, spacing, segments}) {
 
 Spiral.propTypes = {
   sides: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
+  size: PropTypes.number.isRequired,
   spacing: PropTypes.number.isRequired,
   segments: PropTypes.arrayOf(PropTypes.node).isRequired
 };
@@ -85,36 +83,21 @@ export default function Test() {
       textTransform="uppercase"
     >
       <Spiral
+        size={550}
         sides={6}
-        width={500}
         spacing={100}
         segments={[
-          '1',
-          '2',
-          '3',
-          '4',
-          '5',
-          '6',
-          '7',
-          '8',
-          '9',
-          '10',
-          '11',
-          '12',
-          '13',
-          '14',
-          '15',
-          '16',
-          '17',
-          '18',
-          '19',
-          '20',
-          '21',
-          '22',
-          '23',
-          '24',
-          '25',
-          '26'
+          'canelo is the',
+          'best in the',
+          'world right',
+          'now. he might',
+          'be able',
+          'to hit',
+          'him dude',
+          'tune in',
+          'may 19',
+          '2020',
+          'trev.tv'
         ]}
       />
     </Center>
