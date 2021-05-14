17d5674ed5c392c82368e6baf2c3516a9904a6cf
Trevor Blades <tdblades@gmail.com>
2021-05-13 21:15:26 -0700

    Add header

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,6 +1,14 @@
 import React, {useMemo, useState} from 'react';
 import Spiral from 'react-spiral';
-import {Box, Center, Circle, Heading, Text, useTheme} from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Circle,
+  Flex,
+  Heading,
+  Text,
+  useTheme
+} from '@chakra-ui/react';
 import {useMount, useWindowScroll, useWindowSize} from 'react-use';
 
 export default function Test() {
@@ -23,6 +31,37 @@ export default function Test() {
 
   return (
     <>
+      <Flex
+        h="12"
+        px="4"
+        color="white"
+        align="center"
+        as="header"
+        pos="fixed"
+        top="0"
+        zIndex="1"
+      >
+        <Circle
+          size="6"
+          mr="2"
+          borderWidth="2px"
+          borderColor="current"
+          fontFamily="heading"
+          fontWeight="semibold"
+          lineHeight="none"
+          fontSize="xl"
+        >
+          t
+        </Circle>
+        <Heading
+          as="h1"
+          fontSize="2xl"
+          letterSpacing="tighter"
+          lineHeight="none"
+        >
+          Trevor Blades
+        </Heading>
+      </Flex>
       <Box
         height="300vh"
         color="white"
@@ -33,52 +72,52 @@ export default function Test() {
         ]})`}
       >
         <Center
+          key={now}
           height="100vh"
           fontWeight="bold"
+          fontFamily="heading"
           textTransform="uppercase"
           position="sticky"
           top="0"
           overflow="hidden"
         >
-          <div key={now}>
-            <div style={{transform: `scale(${1 + y / height / 4})`}}>
-              <Spiral
-                fontSize={30}
-                boxSize={600}
-                sides={sides}
-                spacing={100}
-                segments={[
-                  "I'm a web",
-                  'developer',
-                  'who enjoys',
-                  'solving',
-                  'puzzles',
-                  'with',
-                  'JS and',
-                  'CSS.',
-                  'Check',
-                  'out',
-                  'some of',
-                  'my work',
-                  'below'
-                ]}
-              />
-            </div>
-            <Circle
-              bgColor="black"
-              pos="absolute"
-              top="50%"
-              left="50%"
-              style={{
-                width: circleSize,
-                height: circleSize,
-                transform: `translate(-50%, -50%) scale(${Math.min(
-                  1,
-                  Math.max(0, y - height) / height
-                )})`
-              }}
+          <div style={{transform: `scale(${1 + y / height / 4})`}}>
+            <Spiral
+              fontSize={30}
+              boxSize={600}
+              sides={sides}
+              spacing={100}
+              segments={[
+                "I'm a web",
+                'developer',
+                'who enjoys',
+                'solving',
+                'puzzles',
+                'with',
+                'JS and',
+                'CSS.',
+                'Check',
+                'out',
+                'some of',
+                'my work',
+                'below'
+              ]}
             />
           </div>
+          <Circle
+            bgColor="black"
+            pos="absolute"
+            top="50%"
+            left="50%"
+            style={{
+              width: circleSize,
+              height: circleSize,
+              transform: `translate(-50%, -50%) scale(${Math.min(
+                1,
+                Math.max(0, y - height) / height
+              )})`
+            }}
+          />
         </Center>
       </Box>
       <Box h="100vh" px="10" bgColor="black" color="white" id="about">
