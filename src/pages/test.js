aac88e20e3449b1fbcecbc7d66986b3f9f6ad22b
Trevor Blades <tdblades@gmail.com>
2021-05-19 20:04:50 -0700

    Add a debug mode

--- src/pages/test.js
+++ src/pages/test.js
@@ -6,9 +6,11 @@ import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import {
   Box,
   Center,
+  Checkbox,
   Circle,
   Code,
   DarkMode,
+  Flex,
   Grid,
   Heading,
   Link,
@@ -27,6 +29,7 @@ SyntaxHighlighter.registerLanguage('jsx', jsx);
 export default function Test() {
   const {colors} = useTheme();
   const [now, setNow] = useState(Date.now());
+  const [debug, setDebug] = useState(false);
 
   useMount(() => setNow(Date.now()));
 
@@ -34,12 +37,15 @@ export default function Test() {
   const {width, height} = useWindowSize();
 
   const sides = useMemo(() => 3 + Math.floor(y / (height / 2)), [y, height]);
-  const circleSize = useMemo(() => {
+
+  const {diameter, interiorAngle} = useMemo(() => {
     // http://mathcentral.uregina.ca/QQ/database/QQ.09.06/s/benneth1.html
     // https://courses.lumenlearning.com/boundless-algebra/chapter/trigonometry-and-right-triangles/
     const diameter = Math.sqrt(width ** 2 + height ** 2);
-    const vmax = Math.max(width, height);
-    return 100 * (diameter / vmax) + 'vmax';
+    return {
+      diameter,
+      interiorAngle: Math.acos(width / diameter)
+    };
   }, [width, height]);
 
   return (
@@ -58,16 +64,13 @@ export default function Test() {
           colors.blue[500]
         ]})`}
       >
-        <Center
-          height="100vh"
-          fontWeight="bold"
-          fontFamily="heading"
-          textTransform="uppercase"
-          position="sticky"
-          top="0"
-          overflow="hidden"
-        >
-          <div style={{transform: `scale(${1 + y / height / 4})`}}>
+        <Center height="100vh" position="sticky" top="0" overflow="hidden">
+          <Box
+            fontWeight="bold"
+            fontFamily="heading"
+            textTransform="uppercase"
+            style={{transform: `scale(${1 + y / height / 4})`}}
+          >
             <Spiral
               fontSize={30}
               boxSize={600}
@@ -89,25 +92,92 @@ export default function Test() {
                 'below'
               ]}
             />
-          </div>
+          </Box>
           <Circle
             bgColor="black"
             pos="absolute"
             top="50%"
             left="50%"
             style={{
-              width: circleSize,
-              height: circleSize,
+              width: diameter,
+              height: diameter,
               transform: `translate(-50%, -50%) scale(${Math.min(
                 1,
                 Math.max(0, y - height) / height
               )})`
             }}
-          />
+          >
+            {debug && (
+              <Flex
+                direction="column"
+                w="100vw"
+                h="100vh"
+                borderWidth="2px"
+                borderLeftColor="green.500"
+                borderBottomColor="blue.500"
+                pos="relative"
+              >
+                <Box fontSize="2xl" fontWeight="medium">
+                  <Box
+                    h={1 / 2}
+                    bgColor="red.500"
+                    transform="rotate(20deg)"
+                    transformOrigin="top left"
+                    textAlign="center"
+                    lineHeight="taller"
+                    style={{
+                      width: diameter,
+                      transform: `translateY(-50%) rotate(${interiorAngle}rad)`
+                    }}
+                  >
+                    <chakra.span color="red.500">diameter</chakra.span> = √(
+                    <chakra.span color="green.500">a</chakra.span>
+                    <sup>2</sup> + <chakra.span color="blue.500">b</chakra.span>
+                    <sup>2</sup>)
+                  </Box>
+                  <Box
+                    px="2"
+                    pos="absolute"
+                    top="50%"
+                    left="0"
+                    transform="translateY(-50%)"
+                  >
+                    a
+                  </Box>
+                  <Box
+                    py="2"
+                    lineHeight="none"
+                    pos="absolute"
+                    bottom="0"
+                    left="50%"
+                    transform="translateX(-50%)"
+                  >
+                    b
+                  </Box>
+                </Box>
+                <Box p="10" mt="auto" w="50%">
+                  <Text mb="4">
+                    To fill up the screen at the right time, the circle must
+                    have a diameter relative to the height and width of the
+                    browser viewport. Now I only need to scale the circle up
+                    from 0 to 1 in conjunction with the window scroll position.
+                  </Text>
+                  <SyntaxHighlighter language="jsx" style={a11yDark}>
+                    {outdent`
+                      const {y} = useWindowScroll();
+                      const {width, height} = useWindowSize();
+                      const circleDiameter = Math.sqrt(width ** 2 + height ** 2);
+                      const circleScale = Math.min(1, y / height);
+                    `}
+                  </SyntaxHighlighter>
+                </Box>
+              </Flex>
+            )}
+          </Circle>
         </Center>
       </Box>
       <DarkMode>
-        <Box px="10" bgColor="black" color="white">
+        <Box p="10" bgColor="black" color="white">
           <Grid templateColumns="2fr 1fr">
             <Stack spacing="20">
               <div id="about">
@@ -125,6 +195,12 @@ export default function Test() {
               </div>
             </Stack>
             <Stack spacing="4">
+              <Checkbox
+                isChecked={debug}
+                onChange={event => setDebug(event.target.checked)}
+              >
+                Debug mode
+              </Checkbox>
               <Text>
                 The spiral text on the homepage was made using{' '}
                 <Code>react-spiral</Code>. You can learn how to use it and read
