94a5111a5b57db6d42dc62abf858386e8c15e78d
Trevor Blades <tdblades@gmail.com>
2021-09-29 22:26:20 -0700

    Proofread and organize madness components

---
+++ src/components/Madness/CaterpillarDemo.js
@@ -0,0 +1,57 @@
+import Demo from './Demo';
+import React, {useState} from 'react';
+import Segment from './Segment';
+
+export function CaterpillarDemo() {
+  const [width, setWidth] = useState(100);
+  const [rotation, setRotation] = useState(0);
+  return (
+    <Demo
+      sliders={[
+        {
+          label: 'Width',
+          min: 50,
+          max: 300,
+          value: width,
+          onChange: setWidth,
+          formatValue: value => value + 'px'
+        },
+        {
+          label: 'Rotation',
+          min: 0,
+          max: 120,
+          value: rotation,
+          onChange: setRotation,
+          formatValue: value => value + '°'
+        }
+      ]}
+    >
+      <div
+        style={{
+          height:
+            width +
+            // get the height of the second segment when it's turned 45deg
+            // it is essential the hypoteneuse of a right triangle where both
+            // of the other sides are equal
+            Math.sqrt(width ** 2 / 2)
+        }}
+      >
+        <Segment value="first" color="red.500" width={width} rotation={0}>
+          <Segment
+            value="second"
+            color="blue.500"
+            width={width}
+            rotation={rotation}
+          >
+            <Segment
+              value="third"
+              color="green.500"
+              width={width}
+              rotation={rotation}
+            />
+          </Segment>
+        </Segment>
+      </div>
+    </Demo>
+  );
+}
---
+++ src/components/Madness/LabelledSlider.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import identity from 'lodash/identity';
+import {
+  Box,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  chakra
+} from '@chakra-ui/react';
+
+export default function LabelledSlider({
+  label,
+  value,
+  formatValue = identity,
+  ...props
+}) {
+  return (
+    <Box w={200}>
+      <chakra.label fontFamily="mono" fontSize="sm">
+        {label}: {formatValue(value)}
+        <div>
+          <Slider value={value} {...props}>
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+        </div>
+      </chakra.label>
+    </Box>
+  );
+}
+
+LabelledSlider.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  formatValue: PropTypes.func
+};
