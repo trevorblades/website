cb65e6e18b6b8bbdd3fed46a75a22ac999c64309
Trevor Blades <tdblades@gmail.com>
2021-10-24 14:22:54 -0700

    Add copy button to code blocks

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -2,34 +2,51 @@ import Highlight, {Prism} from 'prism-react-renderer';
 import PropTypes from 'prop-types';
 import React from 'react';
 import dracula from 'prism-react-renderer/themes/dracula';
-import {chakra} from '@chakra-ui/react';
+import {Box, Button, DarkMode, chakra, useClipboard} from '@chakra-ui/react';
+import {FiClipboard} from 'react-icons/fi';
 
 export default function CodeBlock({children, className}) {
-  const language = className.replace(/language-/, '');
+  const {hasCopied, onCopy} = useClipboard(children);
   return (
     <Highlight
       Prism={Prism}
       theme={dracula}
       code={children.trim()}
-      language={language}
+      language={className.replace(/language-/, '')}
     >
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <chakra.pre
-          className={className}
-          style={style}
-          p={4}
-          fontSize="smaller"
-          rounded="md"
-          fontFamily="mono"
-        >
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({line, key: i})}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({token, key})} />
-              ))}
-            </div>
-          ))}
-        </chakra.pre>
+        <Box pos="relative">
+          <chakra.pre
+            className={className}
+            style={style}
+            p={4}
+            fontSize="smaller"
+            rounded="md"
+            fontFamily="mono"
+            overflow="auto"
+          >
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({line, key: i})}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({token, key})} />
+                ))}
+              </div>
+            ))}
+          </chakra.pre>
+          <DarkMode>
+            <Button
+              pos="absolute"
+              top="2"
+              right="2"
+              size="xs"
+              leftIcon={<FiClipboard />}
+              color="white"
+              onClick={onCopy}
+            >
+              {hasCopied ? 'Copied!' : 'Copy'}
+            </Button>
+          </DarkMode>
+        </Box>
       )}
     </Highlight>
   );
