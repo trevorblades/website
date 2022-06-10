7096788779a80dd32a6be87003f095acd4fc17e3
Trevor Blades <tdblades@gmail.com>
2022-06-10 01:39:23 -0700

    Link the pages

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { Page } from "rakkasjs";
+import { Link, Page } from "rakkasjs";
 
 const HomePage: Page = () => {
   return (
     <main>
       <h1>Hello world!</h1>
-      <p>Welcome to Rakkas.JS demo page.</p>
       <p>
-        Try editing <code>src/pages/page.jsx</code> to get started or go to the{" "}
-        <a href="https://rakkasjs.org" target="_blank" rel="noreferrer">
-          website
-        </a>
-        .
+        Check out <Link href="/about">this page</Link> per chance.
       </p>
     </main>
   );
