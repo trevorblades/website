a062e69a3f7ddfbd8926a847db91de029a3428fe
Trevor Blades <tdblades@gmail.com>
2023-12-22 15:29:55 -0800

    Add google icon

---
+++ src/components/UserStatus.tsx
@@ -0,0 +1,24 @@
+import { Button } from "react-daisyui";
+import { FcGoogle } from "react-icons/fc";
+
+type UserStatusProps = {
+  isLoggedIn: boolean;
+};
+
+export const UserStatus = ({ isLoggedIn }: UserStatusProps) => {
+  return isLoggedIn ? (
+    <Button size="sm" color="accent" tag="a" href="/api/logout">
+      Log out
+    </Button>
+  ) : (
+    <Button
+      size="sm"
+      color="neutral"
+      tag="a"
+      href="/api/oauth/google"
+      startIcon={<FcGoogle />}
+    >
+      Sign in with Google
+    </Button>
+  );
+};
