b7231f60bda5faa609aeeb56e47531be29f93241
Trevor Blades <tdblades@gmail.com>
2023-12-26 19:16:00 -0800

    Add ga via partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,22 @@
 
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [
+    react(),
+    tailwind(),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: cloudflare(),
   vite: {
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_THIS_IS_TEST: string;
   readonly GOOGLE_CLIENT_ID: string;
   readonly GOOGLE_CLIENT_SECRET: string;
   readonly GOOGLE_REDIRECT_URI: string;
